25723459d80231e555ecd12448d439896f1b8380
Bartosz Stryk <85528859+strykb@users.noreply.github.com>
2023-05-27 18:25:56 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarTrader.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
